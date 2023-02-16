1712a4f3846a66250eb3265ad339b8c3398de955
Sukhendra S Lodhi <95135906+sukhendralodhi@users.noreply.github.com>
2023-02-16 15:07:10 +0000

    find positive number

--- .ccls-cache/@home@runner@all-example/main.cpp
+++ .ccls-cache/@home@runner@all-example/main.cpp
@@ -20,11 +20,36 @@ int main() {
   cout << num1 << endl << num2 << endl;
   */
 
+  // swap number without using third variable
+
+  // int num1, num2;
+  // cout << "Enter the number 1 and 2: ";
+  // cin >> num1 >> num2;
+
+  // cout << "Before swap: " << endl << num1 << endl << num2 << endl;
+
+  // num1 = num1 + num2;
+  // num2 = num1 - num2;
+  // num1 = num1 - num2;
+
+  // cout << "After swap: " << endl << num1 << endl << num2 << endl;
+
   // square of given number
+  /*
   int num;
   cout << "Enter the number: ";
   cin >> num;
   cout << "Square of number: " << num * num << endl;
+  */
 
-  
+  // find number is positive or negative
+  int number;
+  cin >> number;
+  if (number > 0) {
+    cout << "Number is positive\n";
+  } else if (number == 0) {
+    cout << "Number is Zero\n";
+  } else {
+    cout << "Number is negative\n";
+  }
 }
--- main.cpp
+++ main.cpp
@@ -22,19 +22,17 @@ int main() {
 
   // swap number without using third variable
 
-  int num1, num2;
-  cout << "Enter the number 1 and 2: ";
-  cin >> num1 >> num2;
+  // int num1, num2;
+  // cout << "Enter the number 1 and 2: ";
+  // cin >> num1 >> num2;
 
-  cout << "Before swap: " << endl << num1 << endl << num2 << endl;
+  // cout << "Before swap: " << endl << num1 << endl << num2 << endl;
 
-  num1 = num1 + num2;
-  num2 = num1 - num2;
-  num1 = num1 - num2;
+  // num1 = num1 + num2;
+  // num2 = num1 - num2;
+  // num1 = num1 - num2;
 
-  cout << "After swap: " << endl << num1 << endl << num2 << endl;
-
-  
+  // cout << "After swap: " << endl << num1 << endl << num2 << endl;
 
   // square of given number
   /*
@@ -44,5 +42,14 @@ int main() {
   cout << "Square of number: " << num * num << endl;
   */
 
-  
+  // find number is positive or negative
+  int number;
+  cin >> number;
+  if (number > 0) {
+    cout << "Number is positive\n";
+  } else if (number == 0) {
+    cout << "Number is Zero\n";
+  } else {
+    cout << "Number is negative\n";
+  }
 }
